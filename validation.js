feb097f00a184c7e45ea70fa4033285ff3c39f1e
Meghana Mohanty <51039811+Meghana-Mohanty@users.noreply.github.com>
2021-05-19 17:24:56 +0530

    Update validation.js

--- validation.js
+++ validation.js
@@ -1,13 +1,9 @@
 function checkButton() {   
 
-    age = parseInt(document.getElementById('a1').value) ;
-    if( document.getElementById('n1').value == "")
+    age = parseInt(document.getElementById("a1").value);
+    if( document.getElementById('a1').value == "")
     {
-        alert("Please enter your name.") ;
-    }
-    else if( document.getElementById('a1').value == "")
-    {
-        alert("Please enter your age.") ;
+        alert("Enter your age") ;
     }
     else if(age < '0' || age > '100')
     {
@@ -15,43 +11,43 @@ function checkButton() {
     }
     else if(!(document.getElementById('g1').checked) && !(document.getElementById('g2').checked) && !(document.getElementById('g3').checked)) 
     {   
-        alert("Select an option.") ; 
+        alert("Select an option for gender") ; 
     }    
     else if(!(document.getElementById('f1').checked) && !(document.getElementById('f2').checked) && !(document.getElementById('f3').checked)) 
     {   
-        alert("Select an option.") ; 
+        alert("Select an option for question 3") ; 
     }    
     else if(!(document.getElementById('c1').checked) && !(document.getElementById('c2').checked)) 
     {   
-        alert("Select an option.") ;
+        alert("Select an option for question 4") ;
     }   
     else if(!(document.getElementById('l1').checked) && !(document.getElementById('l2').checked)) 
     {   
-        alert("Select an option.") ;
+        alert("Select an option for question 5") ;
     } 
     else if(!(document.getElementById('s1').checked) && !(document.getElementById('s2').checked)) 
     {   
-        alert("Select an option.") ;
+        alert("Select an option for question 6") ;
     }   
     else if(!(document.getElementById('b1').checked) && !(document.getElementById('b2').checked)) 
     {   
-        alert("Select an option.") ;
+        alert("Select an option for question 7") ;
     }   
     else if(!(document.getElementById('t1').checked) && !(document.getElementById('t2').checked)) 
     {   
-        alert("Select an option.") ;
+        alert("Select an option for question 8") ;
     }   
     else if(!(document.getElementById('cc1').checked) && !(document.getElementById('cc2').checked) && !(document.getElementById('cc3').checked)) 
     {   
-        alert("Select an option.") ;
+        alert("Select an option for question 9") ;
     }   
     else if(!(document.getElementById('sc1').checked) && !(document.getElementById('sc2').checked) && !(document.getElementById('sc3').checked)) 
     {   
-        alert("Select an option.") ;
+        alert("Select an option for question 10") ;
     }   
     else if(!(document.getElementById('d1').checked) && !(document.getElementById('d2').checked) && !(document.getElementById('d3').checked) && !(document.getElementById('d4').checked) && !(document.getElementById('d5').checked)) 
     {   
-        alert("Select atleast one option.") ;
+        alert("Select atleast one option for question 11") ;
     }   
     else
     {
