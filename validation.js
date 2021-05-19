07a83cd3b625a33e2b86b1fd3a2ccdbd833ea23e
srushti-n <84091607+srushti-n@users.noreply.github.com>
2021-05-19 13:11:36 +0530

    Update validation.js

--- validation.js
+++ validation.js
@@ -1,52 +1,57 @@
 function checkButton() {   
 
+    age = parseInt(document.getElementById('a1').value) ;
     if( document.getElementById('n1').value == "")
     {
-        alert("Enter your name") ;
+        alert("Please enter your name.") ;
     }
     else if( document.getElementById('a1').value == "")
     {
-        alert("Enter your age") ;
+        alert("Please enter your age.") ;
+    }
+    else if(age < '0' || age > '100')
+    {
+        alert("Please enter a valid age.") ;
     }
     else if(!(document.getElementById('g1').checked) && !(document.getElementById('g2').checked) && !(document.getElementById('g3').checked)) 
     {   
-        alert("Select an option") ; 
+        alert("Select an option.") ; 
     }    
     else if(!(document.getElementById('f1').checked) && !(document.getElementById('f2').checked) && !(document.getElementById('f3').checked)) 
     {   
-        alert("Select an option") ; 
+        alert("Select an option.") ; 
     }    
     else if(!(document.getElementById('c1').checked) && !(document.getElementById('c2').checked)) 
     {   
-        alert("Select an option") ;
+        alert("Select an option.") ;
     }   
     else if(!(document.getElementById('l1').checked) && !(document.getElementById('l2').checked)) 
     {   
-        alert("Select an option") ;
+        alert("Select an option.") ;
     } 
     else if(!(document.getElementById('s1').checked) && !(document.getElementById('s2').checked)) 
     {   
-        alert("Select an option") ;
+        alert("Select an option.") ;
     }   
     else if(!(document.getElementById('b1').checked) && !(document.getElementById('b2').checked)) 
     {   
-        alert("Select an option") ;
+        alert("Select an option.") ;
     }   
     else if(!(document.getElementById('t1').checked) && !(document.getElementById('t2').checked)) 
     {   
-        alert("Select an option") ;
+        alert("Select an option.") ;
     }   
     else if(!(document.getElementById('cc1').checked) && !(document.getElementById('cc2').checked) && !(document.getElementById('cc3').checked)) 
     {   
-        alert("Select an option") ;
+        alert("Select an option.") ;
     }   
     else if(!(document.getElementById('sc1').checked) && !(document.getElementById('sc2').checked) && !(document.getElementById('sc3').checked)) 
     {   
-        alert("Select an option") ;
+        alert("Select an option.") ;
     }   
     else if(!(document.getElementById('d1').checked) && !(document.getElementById('d2').checked) && !(document.getElementById('d3').checked) && !(document.getElementById('d4').checked) && !(document.getElementById('d5').checked)) 
     {   
-        alert("Select atleast one option") ;
+        alert("Select atleast one option.") ;
     }   
     else
     {
